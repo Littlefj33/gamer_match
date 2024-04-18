21d6cedeb2352434ad8a81bbdeaa9a35a3ac0b54
mkowalczyk570 <blockman570@gmail.com>
2024-04-18 16:47:37 -0400

    Added comments

--- backend/data/steam.js
+++ backend/data/steam.js
@@ -8,6 +8,7 @@ const client = await createClient()
     .connect();
 const apiKey = "C0FE0FB620850FD036A71B7373F47917"
 
+//function to check if a steam user exists and returns their profile data
 export const getSteamUser = async (steamId) => {
     steamId = validation.stringCheck(steamId);
     try {
@@ -28,6 +29,7 @@ export const getSteamUser = async (steamId) => {
     }
 }
 
+//function that grabs ALL of the games in a Steam User's library
 export const getSteamUsersGames = async (emailAddress) =>{
     const dbInfo = await handleErrorChecking(emailAddress)
     const user = dbInfo.user
@@ -58,6 +60,7 @@ export const getSteamUsersGames = async (emailAddress) =>{
     }
 }
 
+//Function that grabs a steam users recently played games in the last two weeks
 export const getRecentlyPlayed = async (emailAddress) =>{
     const dbInfo = await handleErrorChecking(emailAddress)
     const user = dbInfo.user
@@ -88,6 +91,7 @@ export const getRecentlyPlayed = async (emailAddress) =>{
     }
 }
 
+//Function that gets a users top 5 most played games
 export const getTopFiveGames = async (emailAddress) =>{
     const dbInfo = await handleErrorChecking(emailAddress)
     const user = dbInfo.user
@@ -116,6 +120,7 @@ export const getTopFiveGames = async (emailAddress) =>{
     }
 }
 
+//Function that gets a game from a users library
 export const getUserOwnedGame = async (emailAddress, gameToFind) => {
     const cacheExists = await client.exists(emailAddress + ": " +gameToFind)
     if(cacheExists){
@@ -128,6 +133,8 @@ export const getUserOwnedGame = async (emailAddress, gameToFind) => {
 
     const userGames = user.gamesOwned
     let gameFound
+
+    //Validate that the user has the game in their steam library
     userGames.forEach((game) =>{
         let targetGame = game.name.toLowerCase()
         if(targetGame === gameToFind.toLowerCase()){
@@ -143,6 +150,7 @@ export const getUserOwnedGame = async (emailAddress, gameToFind) => {
     
 }
 
+//Function to get all of the players achievements in a game, and returns a list of all achievement names and whether or not they got the achievement
 export const getPlayerAchievmentsForGame = async(emailAddress, gameToFind) =>{
     const dbInfo = await handleErrorChecking(emailAddress)
     const user = dbInfo.user
@@ -190,35 +198,8 @@ export const getPlayerAchievmentsForGame = async(emailAddress, gameToFind) =>{
     }
 }
 
-export const getGameShema = async (gameId) => {
-    const cacheExists = await client.exists(gameId.toString())
-    if(cacheExists){
-        const gameSchema = await client.get(gameId.toString())
-        return JSON.parse(gameSchema)
-    }
-    try{
-        const response = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${apiKey}&appid=${gameId}`)
-        if (response.status === 200) {
-            const data = response.data.game;
-            if(!data){
-                throw new ResourcesError("Achievements not found")
-            }else{
-                
-                let achievementData = []
-                data.availableGameStats.achievements.forEach((achievement) => {
-                    achievementData.push({displayName: achievement.displayName, apiName: achievement.name})
-                })
-                await client.set(gameId.toString(), JSON.stringify(achievementData))
-                await client.expire(gameId.toString(), 3600) //set with one hour expire time in case the game recieves an update with new achievements
-                return achievementData
-            }
-        }
-    }catch(e){
-        throw new ResourcesError("Cannot get game schema")
-    }
-}
-
-export const matchTwoUsersOnGame = async (user1emailAddress, user2emailAddress) => {
+//Function to get all games both users own
+export const matchTwoUsersOnLibrary = async (user1emailAddress, user2emailAddress) => {
     const user1OwnedGames = await retrieveGamesOwnedFromDb(user1emailAddress);
     const user2OwnedGames = await retrieveGamesOwnedFromDb(user2emailAddress);
 
@@ -234,6 +215,7 @@ export const matchTwoUsersOnGame = async (user1emailAddress, user2emailAddress) 
     return matchingGames
 }
 
+//Finds the top 3 users you share the most games with
 export const findTop3MatchesOnLibrary = async (emailAddress)=>{
     const dbInfo = await handleErrorChecking(emailAddress)
     const user = dbInfo.user
@@ -243,7 +225,7 @@ export const findTop3MatchesOnLibrary = async (emailAddress)=>{
     const commonLibraries = []
     for(const otherUser of allUsers){
         if(user.emailAddress !== otherUser.emailAddress){
-            const matchingGames = await matchTwoUsersOnGame(user.emailAddress, otherUser.emailAddress)
+            const matchingGames = await matchTwoUsersOnLibrary(user.emailAddress, otherUser.emailAddress)
             commonLibraries.push({
                 username: user.username, 
                 userMatched: otherUser.username,
@@ -256,19 +238,25 @@ export const findTop3MatchesOnLibrary = async (emailAddress)=>{
     return commonLibraries.sort((a, b) => b.numGamesShared - a.numGamesShared).slice(0,3)
 }
 
+//Matches users based on achievements, this one is really messy and comments explain how it works, but this can use some cleanup
 export const matchOnAchievements = async (emailAddress, game, matchType) =>{
     const dbInfo = await handleErrorChecking(emailAddress)
     game = validation.stringCheck(game)
     validation.matchType(matchType)
     const user = dbInfo.user
     const allUsersWithGame = await getAllUsersWhoOwnGame(game);
+
+    //Get all user achievements. This includes names and whether or not it has been achieved
     const userAchievements = await getPlayerAchievmentsForGame(emailAddress, game)
-    const userAchievementStates = await getAchievedStates(userAchievements)
+    const userAchievementStates = await getAchievedStates(userAchievements) 
     const matchedUsers = []
     for(const otherUser of allUsersWithGame){
         if(user.emailAddress !== otherUser.emailAddress){
+            //Get all user achievements. This includes names and whether or not it has been achieved
             const otherUserAchievements = await getPlayerAchievmentsForGame(otherUser.emailAddress, game)
             const otherUserAchievementStates = await getAchievedStates(otherUserAchievements)
+
+            //Find all achievements neither user has, and achievements that one user has and the other doesnt
             const achievementData = await mapAchievementStates(userAchievementStates, otherUserAchievementStates)
             matchedUsers.push({
                 username: user.username,
@@ -281,16 +269,26 @@ export const matchOnAchievements = async (emailAddress, game, matchType) =>{
         }
     }
 
+    //On frontend we will have a 3 ways to sort
+
+    //Say I want to grind achievements with another person
+    //sorts matched users based on highest count that neither player achieved
     if(matchType === 'neitherAchieved'){
         return matchedUsers.sort((userA, userB) =>{
             userB.achievementsNeitherHas.length - userA.achievementsNeitherHas.length
         })
     }
+
+    //Say I want to help someone else get achievements I already have and know how to get
+    //Sorts matched users based on highest count of achievements I have, but the other user doesnt
     else if(matchType === 'iAchieved'){
         return matchedUsers.sort((userA, userB) =>{
             userB.achievementsUserHasOtherDoesnt.length - userA.achievementsUserHasOtherDoesnt.length
         })
     }
+
+    //Say I need help to get achievements i do not have
+    //Sorts matched users based on highest count of achievements the other user has, but I dont
     if(matchType === 'theyAchieved'){
         return matchedUsers.sort((userA, userB) =>{
             userB.achievementsOtherHasUserDoesnt.length - userA.achievementsOtherHasUserDoesnt.length
@@ -298,6 +296,7 @@ export const matchOnAchievements = async (emailAddress, game, matchType) =>{
     }
 }
 
+//Helper function that returns achievements that neither user has, and achievements that one user has and the other doesnt.
 export const mapAchievementStates = async (userStates, otherUserStates) => {
     if(!userStates || !otherUserStates){
         throw new ResourcesError("You must provide both user's achievements")
@@ -324,10 +323,11 @@ export const mapAchievementStates = async (userStates, otherUserStates) => {
         }
     });
 
+    //lets explain these variables in this object
     return{
-        neitherUserAchieved: neitherAchieved, 
-        userAchieved: userAchieved, 
-        otherUserAchieved: otherUserAchieved
+        neitherUserAchieved: neitherAchieved, //Achievements neither user has
+        userAchieved: userAchieved,  //Achievements I have, but the other user doesnt
+        otherUserAchieved: otherUserAchieved //Achievements other user has, but I dont
     }
 }
 
@@ -342,6 +342,7 @@ export const getAchievedStates = async (achievementList)=>{
     return {achieved: achieved, notAchieved: notAchieved}
 }
 
+//helper function to get all users in db that own a particular game
 export const getAllUsersWhoOwnGame = async (gameName) =>{
     const usersCollection = await users();
     const allUsers = await usersCollection.find({}).toArray()
@@ -353,4 +354,34 @@ export const getAllUsersWhoOwnGame = async (gameName) =>{
     })
 
     return usersWithGame
+}
+
+
+//Helper function to get achievement display names
+export const getGameShema = async (gameId) => {
+    const cacheExists = await client.exists(gameId.toString())
+    if(cacheExists){
+        const gameSchema = await client.get(gameId.toString())
+        return JSON.parse(gameSchema)
+    }
+    try{
+        const response = await axios.get(`https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${apiKey}&appid=${gameId}`)
+        if (response.status === 200) {
+            const data = response.data.game;
+            if(!data){
+                throw new ResourcesError("Achievements not found")
+            }else{
+                
+                let achievementData = []
+                data.availableGameStats.achievements.forEach((achievement) => {
+                    achievementData.push({displayName: achievement.displayName, apiName: achievement.name})
+                })
+                await client.set(gameId.toString(), JSON.stringify(achievementData))
+                await client.expire(gameId.toString(), 3600) //set with one hour expire time in case the game recieves an update with new achievements
+                return achievementData
+            }
+        }
+    }catch(e){
+        throw new ResourcesError("Cannot get game schema")
+    }
 }
