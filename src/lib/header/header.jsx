d3bbe96bdc176136dbd210c1665e3e8ba989a10c
Frank Orlando <frankorlando@optonline.net>
2024-04-21 22:30:03 -0400

    Updated folder structure for entire app

--- src/lib/header/header.jsx
+++ src/lib/header/header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="w-full h-16 flex flex-wrap items-center justify-center bg-white z-30 text-black">
+        <div className="w-full h-16 flex flex-wrap items-center justify-center bg-white z-30 text-white">
             <div className="mx-auto h-10 flex flex-wrap justify-center items-center">
                 <Link href={"/"} className="h-full">
                     <div className="w-40 h-full text-center border-r border-black hover:bg-blue-100">
