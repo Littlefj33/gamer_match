b09a17cf3cfa3f41796788c478a33032b4d4c414
twang1905 <twang1905@gmail.com>
2024-04-24 20:01:43 -0400

    seed file fully configured

--- backend/tasks/seed.js
+++ backend/tasks/seed.js
@@ -7,14 +7,12 @@ import {
     getUserOwnedGame,
     getPlayerAchievmentsForGame
 } from "../data/steam.js";
+import {
+    sendFriendRequest,
+    acceptFriendRequest
+} from "../data/friends.js";
 
 const main = async () => {
-    // await registerUser("Scribble", "Scribble@mail.com", "Password123!!!!");
-    // await registerUser("Aero", "Aero@mail.com", "Password123!!!!");
-    // await registerUser("LittleFJ", "LittleFJ@mail.com", "Password123!!!!");
-    // await registerUser("wackyjackie11", "wackyjackie11@mail.com", "Password123!!!!");
-    // console.log("Done adding users");
-    // console.log("Done linking steam account");
     await registerUser("twang", "twang@mail.com", "Password123!!!!");
     await linkSteamAccount("twang@mail.com", "76561198061876066");
     await getSteamUser("76561198061876066");
@@ -23,7 +21,54 @@ const main = async () => {
     await getTopFiveGames("twang@mail.com");
     await getUserOwnedGame("twang@mail.com", "Among Us");
     await getPlayerAchievmentsForGame("twang@mail.com", "Among Us");
+    console.log("1/5 Users Seeded");
 
+    await registerUser("Aero", "aero@mail.com", "Password123!!!!");
+    await linkSteamAccount("aero@mail.com", "76561198160939488");
+    await getSteamUser("76561198160939488");
+    await getSteamUsersGames("aero@mail.com");
+    await getRecentlyPlayed("aero@mail.com");
+    await getTopFiveGames("aero@mail.com");
+    console.log("2/5 Users Seeded");
+
+    await registerUser("CausticLimes", "causticlimes@mail.com", "Password123!!!!");
+    await linkSteamAccount("causticlimes@mail.com", "76561198024306587");
+    await getSteamUser("76561198024306587");
+    await getSteamUsersGames("causticlimes@mail.com");
+    await getRecentlyPlayed("causticlimes@mail.com");
+    await getTopFiveGames("causticlimes@mail.com");
+    console.log("3/5 Users Seeded");
+
+    await registerUser("Shinks", "shinks@mail.com", "Password123!!!!");
+    await linkSteamAccount("shinks@mail.com", "76561198097969047");
+    await getSteamUser("76561198097969047");
+    await getSteamUsersGames("shinks@mail.com");
+    await getRecentlyPlayed("shinks@mail.com");
+    await getTopFiveGames("shinks@mail.com");
+    console.log("4/5 Users Seeded");
+
+    // has no recently played games, so the function will throw an error if this is run
+    // await registerUser("LackofAIR22", "lackofair22@mail.com", "Password123!!!!");
+    // await linkSteamAccount("lackofair22@mail.com", "76561198226833264");
+    // await getSteamUser("76561198226833264");
+    // await getSteamUsersGames("lackofair22@mail.com");
+    // await getRecentlyPlayed("lackofair22@mail.com");
+    // await getTopFiveGames("lackofair22@mail.com");
+    // console.log("5/5 Users Seeded");
+
+    await sendFriendRequest("twang", "Aero");
+    await sendFriendRequest("twang", "CausticLimes");
+    await sendFriendRequest("twang", "Shinks");
+    await sendFriendRequest("Aero", "CausticLimes");
+    console.log("Friend Requests Sent");
+
+    await acceptFriendRequest("Aero", "twang");
+    await acceptFriendRequest("CausticLimes", "twang");
+    await acceptFriendRequest("Shinks", "twang");
+    await acceptFriendRequest("CausticLimes", "Aero");
+    console.log("Friend Requests Accepted");
+
+    console.log("Program has been seeded successfully!");
     process.exit();
 };
 
