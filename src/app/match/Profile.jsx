f830566d31c17e995c5da4f0cb587281601d6501
Frank Orlando <frankorlando@optonline.net>
2024-05-05 21:20:18 -0400

    achievementMatch start

--- src/app/match/Profile.jsx
+++ src/app/match/Profile.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Profile() {
+export default function Profile(userInfo) {
+    const handleAddFriend = (username) => {};
+
     return (
         <div className="w-44 h-44 flex flex-col px-4 py-3 rounded-3xl bg-gradient-to-b from-tangerine to-bittersweet text-black">
             <div className="w-full flex justify-between items-center h-10 overflow-hidden">
-                <div className="text-m font-semibold">Username</div>
+                <div className="text-m font-semibold">{userInfo.username}</div>
 
                 <Link href="/profile" className="border border-black">
                     <Image
--- src/app/match/actions.js
+++ src/app/match/actions.js
@@ -1,1 +1,5 @@
+"use server";
+
 /* Form actions to communicate to backend */
+
+export async function achievementMatch({ matchType, gameName }) {}
