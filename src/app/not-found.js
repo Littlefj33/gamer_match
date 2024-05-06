8fceed249d53aff7a16e1bacf9cafa92c74a9e46
twang1905 <twang1905@gmail.com>
2024-05-06 17:08:03 -0400

    404 page working

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,28 +1,17 @@
-'use client';
+"use client";
 import Header from "@/lib/header/header";
-import { redirect } from "next/navigation";
+import Link from "next/link";
 import "./globals.css";
 
 export default function Custom404() {
-  const handleRedirect = async (e) => {
-    e.preventDefault();
-    try {
-        redirect("/");
-    } catch (error) {
-        alert(error);
-    }
-};
-
-  return (
-    <>
-        <Header />
-        <div className="w-full h-screen bg-platinum text-black">
-            <form onSubmit={handleRedirect}>
-                <button className="button" type="submit">
+    return (
+        <>
+            <Header />
+            <div className="w-full h-screen bg-platinum text-black">
+                <Link href="/">
                     404: Click here to go back to the home page!
-                </button>
-            </form>
-        </div>
-    </>
-);
+                </Link>
+            </div>
+        </>
+    );
 }
