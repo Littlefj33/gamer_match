a0c060749534ff919890c78aa70da9ee8cd0fc5e
markfalletta <markcfalletta@gmail.com>
2024-05-05 21:05:45 -0400

    reverted Profile.jsx

--- src/app/match/Profile.jsx
+++ src/app/match/Profile.jsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import SendButton from "@/lib/friendButton/SendButton";
 
-export default function Profile({sender, recipient}) {
-    let achievements 
+export default function Profile() {
     return (
         <div className="w-44 h-44 flex flex-col px-4 py-3 rounded-3xl bg-gradient-to-b from-tangerine to-bittersweet text-black">
             <div className="w-full flex justify-between items-center h-10 overflow-hidden">
-                <div className="text-m font-semibold"></div>
+                <div className="text-m font-semibold">Username</div>
 
                 <Link href="/profile" className="border border-black">
                     <Image
@@ -29,7 +27,7 @@ export default function Profile({sender, recipient}) {
             </div>
 
             <div className="flex justify-end">
-                <SendButton sender="aero" recipient = "shinks"/>
+                <button className="text-black font-medium">+ Add Friend</button>
             </div>
         </div>
     );
