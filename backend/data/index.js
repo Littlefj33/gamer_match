511fe59bec28161f084a94e4b41c82c2459fa732
Frank Orlando <frankorlando@optonline.net>
2024-05-07 18:09:27 -0400

    Formatting changes

--- backend/data/index.js
+++ backend/data/index.js
@@ -1,6 +1,6 @@
-import * as userDataFunctions from './users.js';
-import * as steamDataFunctions from './steam.js';
-import * as friendDataFunctions from './friends.js';
+import * as userDataFunctions from "./users.js";
+import * as steamDataFunctions from "./steam.js";
+import * as friendDataFunctions from "./friends.js";
 
 export const userData = userDataFunctions;
 export const steamData = steamDataFunctions;
